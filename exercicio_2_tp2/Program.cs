006a7296d83d824a35fa1b520445211040908d4c
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-11-20 12:42:42 -0300

    #Com fins educativos para o Instituto Infnet

--- exercicio_2_tp2/Program.cs
+++ exercicio_2_tp2/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace exercicio_02_tp2
+﻿using System;
+
+namespace exercicio_02_tp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Digite sua data de nascimento: ");
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+
+            DateTime hoje = DateTime.Today;
+
+            DateTime proximoAniversario = new DateTime(
+              hoje.Year,
+              dataNascimento.Month,
+              dataNascimento.Day
+                );
+
+           
         }
     }
 }
