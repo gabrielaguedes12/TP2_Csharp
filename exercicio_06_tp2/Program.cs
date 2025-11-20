1f3a8ece90b8e2f30f2a2fc95f49a513edbac8d8
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-11-20 17:08:24 -0300

    acerto no git

--- exercicio_06_tp2/Program.cs
+++ exercicio_06_tp2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Digite seu peso (kg): ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua altura (m): ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            Console.WriteLine($"\nSeu IMC é: {imc:F2}");
+
+            string classificacao;
+
+            if (imc < 18.5)
+                classificacao = "Abaixo do peso";
+            else if (imc < 25)
+                classificacao = "Peso normal";
+            else if (imc < 30)
+                classificacao = "Sobrepeso";
+            else if (imc < 35)
+                classificacao = "Obesidade Grau I";
+            else if (imc < 40)
+                classificacao = "Obesidade Grau II";
+            else
+                classificacao = "Obesidade Grau III";
+
+            Console.WriteLine($"Classificação: {classificacao}");
         }
     }
 }
