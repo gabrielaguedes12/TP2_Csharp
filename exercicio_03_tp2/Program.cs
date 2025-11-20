6dfa6d6235fc5597efec622e314563a6263397a4
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-11-20 13:39:08 -0300

    acerto no git

--- exercicio_03_tp2/Program.cs
+++ exercicio_03_tp2/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite a primeira data (dd/mm/aaaa): ");
+            DateTime data1 = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segundaa data (dd/mm/aaaa): ");
+            DateTime data2 = DateTime.Parse(Console.ReadLine());
+
+            if (data1 > data2)
+            {
+                DateTime temp = data1;
+                data1 = data2;
+                data2 = temp;
+            }
+
+                int anos = data2.Year - data1.Year;
+                int meses = data2.Month -data1.Month;
+                int dias = data2.Day - data1.Day;
+
+            if (dias < 0)
+            {
+                meses--;
+                dias += DateTime.DaysInMonth(data2.AddMonths(-1).Year, data2.AddMonths(-1).Month);
+            }
+
+            if (meses < 0)
+            {
+                anos--;
+                meses += 12;
+            }
+            TimeSpan intervalo = data2 - data1;
+            int totalDias = intervalo.Days;
+
+            Console.WriteLine("\n===== Diferença Entre as Datas =====");
+            Console.WriteLine($"Anos:  {anos}");
+            Console.WriteLine($"Meses: {meses}");
+            Console.WriteLine($"Dias:  {dias}");
+            Console.WriteLine($"Total em dias: {totalDias}");
         }
     }
 }
--- exercicio_04_pt2/Program.cs
+++ exercicio_04_pt2/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("==Frmulário de Cadastro==");
+
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite seu idade: ");
+            string idade = Console.ReadLine();
+
+            Console.Write("Digite seu telefone: ");
+            string telefone = Console.ReadLine();
+
+            Console.Write("Digite seu e-mail: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("==Cadastro==");
+            Console.WriteLine($"Nome:     {nome}");
+            Console.WriteLine($"Idade:     {idade}");
+            Console.WriteLine($"Telefone:     {telefone}");
+            Console.WriteLine($"E-mail:     {email}");
+
         }
     }
 }
