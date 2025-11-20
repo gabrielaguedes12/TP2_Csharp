d0e9372bf0283be15ae6ac1906a9e0dfed651ae5
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-11-20 17:05:39 -0300

    acerto no git

--- exercicio_05_tp2/Program.cs
+++ exercicio_05_tp2/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("git infernooo");
+            Console.WriteLine("Digite a temperatura em Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+
+            double fahrenheit = celsius * 9/5 + 32;
+            double kelvin = celsius + 273.15;
+
+            Console.WriteLine($"O número em Fahrenheit é: {fahrenheit :F2} °F");
+            Console.WriteLine($"Temperatura em Kelvin: {kelvin:F2} K");
         }
     }
 }
